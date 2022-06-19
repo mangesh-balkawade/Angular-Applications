a5531e1d31e06237c72b82d8371a18dfaa8ec468
Mangesh Balkawade <101330511+mangesh-balkawade@users.noreply.github.com>
2022-06-19 13:54:55 +0530

    Src of modules with LazyLoading

---
+++ src ModulesWithLazyLoading/app/admin/admin-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AdemoComponent } from './ademo/ademo.component';
+
+const routes: Routes = [
+  {path:'',component:AdemoComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+
+export class AdminRoutingModule { }
