590c676144566518fa954abf4100bbe2996a6794
mangesh-balkawade <mangeshbalkawade219@gmail.com>
2023-02-28 17:57:41 +0530

    Event Hub Project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,37 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component'
+import { EventsComponent } from './events/events.component';
+import { SpecialEventsComponent } from './special-events/special-events.component';
+import { AuthGuard } from './auth.guard'; // Can Activate 
+import { FormComponent } from './form/form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/events',
+    pathMatch: 'full'
+  },
+  {
+    path: 'events',
+    component: EventsComponent
+  },
+  {
+    path: 'special',
+    // Interface [Gurad File Name AuthGuard Auth he user define nav ahe]
+    canActivate: [AuthGuard], // AuthGuard file name Line 6
+    // Jar key nasel tar tithunach log in sathi gelay router.navigate karun
+    component: SpecialEventsComponent
+    
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'form', component: FormComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
